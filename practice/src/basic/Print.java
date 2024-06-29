3aba89a8d093f97719ba474d6581e83cd8758d2e
shakir872 <mdshakirmumtaz74@gmail.com>
2024-06-29 12:00:06 +0530

    change in print.java

--- practice/src/basic/Print.java
+++ practice/src/basic/Print.java
@@ -5,6 +5,10 @@ public class Print {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Everyone");
+		int a = 65;
+		int b = 55;
+		System.out.println("the sum of a and b is");
+		System.out.println(a+b);
 
 	}
 
