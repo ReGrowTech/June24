f051c21338a54febfd48e1640b8cd36db6bc4563
Omar Farooque <Omar-Farooque>
2024-07-03 18:05:08 +0530

    basic program of git

--- practice/src/basic/BasicGit.java
+++ practice/src/basic/BasicGit.java
@@ -2,8 +2,14 @@ package basic;
 
 public class BasicGit {
    public static void main(String args[]) {
-	   String name = "ReGrow";
+	  /* String name = "ReGrow";
 	   int yearfrom = 2020;
 	   System.out.println("hello,there institution name is:"+name+"established from:"+yearfrom);
+	  */
+	   
+	  
+	   int num = 57;
+	   String x = String.valueOf(num);
+	   System.out.println(x);
    }
 }
