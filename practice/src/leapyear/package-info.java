9b75c104c37b48ec6e6e7d2e6ac46d863beafbd0
ashaz06 <md.shahbaz121219@gmail.com>
2024-07-07 11:55:55 +0530

    Changes made in the leap year programme of the main branch

--- practice/src/leapyear/package-info.java
+++ practice/src/leapyear/package-info.java
@@ -1,1 +1,11 @@
 package leapyear;
+public class leap_yr{
+	public static void main(String args[]) {
+		int yr=2024;
+		if(((yr%100==0)&&(yr%400==0))||((yr%100=0)&&(yr%4==0)) {
+			System.out.println("Leap year");
+		}else {
+			System.out.println("Not a leap year");
+		}
+	}
+}
