e0e5cbed00464687270f2214975198894993384d
Omar Farooque <Omar-Farooque>
2024-07-15 17:57:40 +0530

    my work omarfarooque

--- practice/src/basic_Omar/Convert_Ascii_Char_Int.java
+++ practice/src/basic_Omar/Convert_Ascii_Char_Int.java
@@ -3,7 +3,7 @@ package basic_Omar;
 public class Convert_Ascii_Char_Int {
 	
 public static void main(String args[]) {
-	char ch ='{';
+	char ch ='&';
 	int num;
 	num = ch;
 	System.out.println(num);
