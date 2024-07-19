b6cb063a0d476ba79023f6f8aee7585bef341846
SweetyXaxa26 <sweetyxaxa28@gmail.com>
2024-07-19 09:25:12 +0530

    Complete

---
+++ practice/src/shweta_prg/PrintStatement.java
@@ -0,0 +1,13 @@
+package shweta_prg;
+
+public class PrintStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My first code in Git_Hub");
+		
+
+	}
+
+}
