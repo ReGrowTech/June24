7d703d1bc0ae8ac488987d03a4e10cb7fd02987c
SweetyXaxa26 <sweetyxaxa28@gmail.com>
2024-07-19 12:59:22 +0530

    practice java

---
+++ practice/src/programing_Capsules_sweety/Inc_Dec_operator5_capsule_5.java
@@ -0,0 +1,21 @@
+package programing_Capsules_sweety;
+
+public class Inc_Dec_operator5_capsule_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a= 12;
+		int b=8;
+		
+		a*= ++a/6 + b++ % 3;
+		// a = 12*( 13/6 + b++%3) 
+		// a= 12*(2 + 2)
+		//a= 12*4
+		
+		System.out.println(a);
+		
+
+	}
+
+}
