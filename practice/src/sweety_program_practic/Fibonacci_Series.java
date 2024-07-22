795e3e625c600ccc31401aef83d5b0ec32125413
SweetyXaxa26 <sweetyxaxa28@gmail.com>
2024-07-22 09:31:16 +0530

    practice java

---
+++ practice/src/sweety_program_practic/Fibonacci_Series.java
@@ -0,0 +1,24 @@
+package sweety_program_practic;
+
+public class Fibonacci_Series {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		int num = 8;
+		int fr=0;
+		int se=1;
+		int next_n;
+		System.out.print(fr + " "+se +" ");
+		
+		for(int i=1;i<=num;i++)
+		{
+		next_n=fr+se;
+		System.out.print(next_n +" ");
+		
+		fr=se;
+		se=next_n;
+		}
+	}
+
+}
