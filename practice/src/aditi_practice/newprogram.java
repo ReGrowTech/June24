1bb12a9ceb7b24d7418826ff42e4d140b3dea68b
aditimishra1112 <aditiabantika22@gmail.com>
2024-07-28 00:20:54 +0530

    New Commit

---
+++ practice/src/aditi_practice/newprogram.java
@@ -0,0 +1,27 @@
+package aditi_practice;
+
+class abc{
+	 public void check(int num) {
+		 
+		 if(num > 1) {
+			 System.out.println("positive number");
+		 }
+		 else {
+			 System.out.println("negative number");
+		 }
+		
+	}
+}
+
+public class newprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		abc a = new abc();
+		a.check(5);
+		
+		
+
+	}
+
+}
--- practice/src/leapyear/Leapyr.java
+++ practice/src/leapyear/Leapyr.java
@@ -1,8 +1,8 @@
 package leapyear;
-public class leap_yr{
+public class Leapyr{
 	public static void main(String args[]) {
 		int yr=2024;
-		if(((yr%100==0)&&(yr%400==0))||((yr%100=0)&&(yr%4==0)) {
+		if((yr%100==0)&&(yr%400==0) || (yr%100==0)&&(yr%4==0)) {
 			System.out.println("Leap year");
 		}else {
 			System.out.println("Not a leap year");
