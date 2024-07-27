978a0e71b1cffc5ac5ce69fa5fb79d493f991237
aditimishra1112 <aditiabantika22@gmail.com>
2024-07-28 00:32:21 +0530

    New Code

---
+++ practice/src/aditi_practice/prog1.java
@@ -0,0 +1,12 @@
+package aditi_practice;
+
+public class prog1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("practice program 2 ");
+
+	}
+
+}
