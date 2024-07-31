1d5ba24fca2583c04e75d7b7601eb24451b3e420
SweetyXaxa26 <sweetyxaxa28@gmail.com>
2024-07-31 18:17:45 +0530

    by Sweety

---
+++ practice/src/capsule_4_by_sweety/Greatest_of2_numberUsing_ternary.java
@@ -0,0 +1,15 @@
+package capsule_4_by_sweety;
+
+public class Greatest_of2_numberUsing_ternary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=89;
+		int b=789;
+		
+		int res =(a>b)? a : b;
+		System.out.println(res +" result ");
+	}
+
+}
