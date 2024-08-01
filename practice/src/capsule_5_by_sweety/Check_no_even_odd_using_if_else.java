6ad1ca51a916d3994c3a440c90d89a3fed29e1a7
SweetyXaxa26 <sweetyxaxa28@gmail.com>
2024-08-01 23:12:42 +0530

    sweety work

---
+++ practice/src/capsule_5_by_sweety/Check_no_even_odd_using_if_else.java
@@ -0,0 +1,20 @@
+package capsule_5_by_sweety;
+
+public class Check_no_even_odd_using_if_else {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num = 5;
+		
+		if(num%2==0)
+		{
+			System.out.println("Even ");
+		}
+		else
+		{
+			System.out.println("Odd");
+		}
+	}
+
+}
---
+++ practice/src/capsule_5_by_sweety/Smallest_2_no_using_if_else.java
@@ -0,0 +1,21 @@
+package capsule_5_by_sweety;
+
+public class Smallest_2_no_using_if_else {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=90;
+		int b=400;
+		
+		if(a<b)
+		{
+			System.out.println(a +" a  Is the Smallest of  no ");
+		}
+		else
+		{
+			System.out.println(b + " b Is the Smallest  of  no ");
+		}
+	}
+
+}
